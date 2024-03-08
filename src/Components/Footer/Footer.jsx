03381291085868bcde4a57e5eccff2b3f9f94fd6
ammad-amin-1999313 <madekhan007@gmail.com>
2024-03-08 16:00:31 +0500

    login ammad

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,5 +1,4 @@
-// import React from "react";
-import SectionWrapper from "../Wrapper/SectionWrapper";
+import React from "react";
 import prelogo from "../../assets/png/prelogo.png";
 import { Link } from "react-router-dom";
 import fb from "../../assets/png/fb.png";
@@ -25,15 +24,6 @@ const useLink = [
   },
 ];
 
-// const contact = [
-//   { name: "info@website.com" },
-//   {
-//     name: "1-100-100-300",
-//   },
-//   {
-//     name: "3500 Root Main Rd  Street Root, CA ",
-//   },
-// ];
 const icons = [
   { image: fb, link: "#" },
   { image: instagram, link: "#" },
@@ -44,19 +34,19 @@ const Footer = () => {
   return (
     <>
       <div className="bg-[#D9D9D9]">
-        <div className="flex gap-[290px] pt-[60px] mx-auto w-[85%] ">
+        <div className=" flex flex-col  md:flex-row md:gap-[290px] pt-[60px] mx-auto w-[85%] ">
           <div className="">
             <img src={prelogo} alt="logo" />
-            <p className="pt-[30px]">
+            <p className="pt-[30px] font-sans	">
               Lorem Ipsum is simply dummy text of the <br /> printing and
               typesetting industry.
             </p>
           </div>
           <div>
-            <h1 className="text-[#49BBBD] text-[25px] pb-[30px] ">
+            <h1 className="text-[#49BBBD] text-[25px] py-4 md:py-0 font-sans	 md:pb-[30px] ">
               Useful Links
             </h1>
-            <div className="flex flex-col text-[18px] gap-[15px] font-regular items-center">
+            <div className="flex flex-col font-sans	 text-[18px] gap-[15px] font-regular md:items-center">
               {useLink.map((link, index) => (
                 <Link key={index} to={link.link}>
                   {link.name}
@@ -64,20 +54,20 @@ const Footer = () => {
               ))}
             </div>
           </div>
-          <div className=" flex flex-col  gap-[15px]">
-            <h1 className="text-[#49BBBD] text-[25px] text-center pb-[17px] ">
+          <div className=" flex flex-col gap-[11px] md:gap-[15px] font-sans	">
+            <h1 className="text-[#49BBBD] text-[25px] md:text-center pt-3 md:pt-0 md:pb-[17px] ">
               Contact Us
             </h1>
-            <h4 className="text-[18px] font-regular text-center">
+            <h4 className="text-[18px] font-regular  md:text-center">
               info@website.com
             </h4>
-            <h4 className="text-[18px] font-regular text-center">
+            <h4 className="text-[18px] font-regular md:text-center">
               1-100-100-300
             </h4>
-            <h4 className="text-[18px] font-regular text-center">
+            <h4 className="text-[18px] font-regular md:text-center">
               3500 Root Main Rd <br /> Street Root, CA
             </h4>
-            <div className="flex justify-between">
+            <div className="flex pt-2 md:pt-0 justify-between">
               {icons.map((icon, index) => (
                 <Link key={index}>
                   <img src={icon.image} alt="icons" />
@@ -88,7 +78,9 @@ const Footer = () => {
         </div>
       </div>
 
-      <div className="bg-[#d9d9d9]">© Copyright websiteurl.com</div>
+      <div className=" bg-[#d9d9d9] pt-[30px] font-sans	">
+        <div className="w-[85%] mx-auto ">© Copyright websiteurl.com</div>
+      </div>
     </>
   );
 };
--- src/Components/RegistrationDetails/Login.jsx
+++ src/Components/RegistrationDetails/Login.jsx
@@ -1,17 +1,55 @@
-// import React from "react";
-import SectionWrapper from "../Wrapper/SectionWrapper";
+import React from "react";
 import cta from "../../assets/png/CTA.png";
-
+import { HiArrowLeft } from "react-icons/hi2";
 const Login = () => {
   return (
-    <SectionWrapper>
-      <div className="flex">
-        <div className="height-[100vh]">
-          <img src={cta} width={747} height={747} alt="cta coaching" />
+    <div className="w-full">
+      <div className="md:w-[80%] w-[90%] mx-auto">
+        <div>
+          <HiArrowLeft />
+          <span>Login</span>
         </div>
-        <div></div>
+
+        <div className="flex">
+          <div className="">
+            <img src={cta} width={589} height={550} alt="cta coaching" />
+          </div>
+          <div>
+            <h1>Welcome to Lorem</h1>
+            <div>
+              <button>Login</button>
+              <button>Register</button>
+            </div>
+            <p>
+              Lorem Ipsum is simply dummy text of the printing and <br />{" "}
+              typesetting industry.
+            </p>
+            <div>
+              <label htmlFor="name">User name</label>
+              <input type="text" placeholder="Enter your User name" />
+            </div>
+            <div>
+              <label htmlFor="password">Password</label>
+              <input
+                type="password"
+                name="password"
+                placeholder="Enter your Password"
+              />
+            </div>
+            <div>
+              <div>
+                <input type="checkbox" />
+                <label htmlFor="remember me">Remember me</label>
+              </div>
+              <div>
+                <button>Forgot Password ? </button>
+              </div>
+            </div>
+            <button>Login</button>
+          </div>
+        </div>
       </div>
-    </SectionWrapper>
+    </div>
   );
 };
 
